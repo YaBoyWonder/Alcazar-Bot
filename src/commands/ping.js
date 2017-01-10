491a6a4a05a1681576f812e72911b3051cc9ab61
YaBoyWonder <rahulchalumuri54@gmail.com>
2017-01-10 08:49:22 -0500

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,4 +1,4 @@
-//for this to work for you, you need to install indev
+
 exports.run = function(client, message, args) {
 	message.channel.sendMessage(`Pong! \`${Date.now() - message.createdTimestamp} ms\``)
 };
